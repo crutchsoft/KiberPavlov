c942d534fb1bfd589a34a47816d0d0e34b641be4
kiberpasha <39lamp@gmail.com>
2023-04-01 02:43:13 +0200

    Fix server target typo

--- Source/KiberPavlovServer.Target.cs
+++ Source/KiberPavlovServer.Target.cs
@@ -1,13 +1,12 @@
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class KiberPavlov : TargetRules
+public class KiberPavlovServerTarget : TargetRules //Change this line according to the name of your project
 {
-    public KiberPavlov(TargetInfo Target) : base(Target)
+    public KiberPavlovServerTarget(TargetInfo Target) : base(Target) //Change this line according to the name of your project
     {
         Type = TargetType.Server;
         DefaultBuildSettings = BuildSettingsVersion.V2;
-        ExtraModuleNames.Add("KiberPavlov"); 
+        ExtraModuleNames.Add("KiberPavlov"); //Change this line according to the name of your project
     }
 }
